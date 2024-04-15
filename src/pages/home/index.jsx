07cfe5f961360164e0c5dd46720952d7c4a22396
mmulyana <mulyan.t20@gmail.com>
2024-04-15 08:26:07 +0700

    feat: add link in homepage and new routes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
-    return (
-        <>
-            <p>Home</p>
-        </>
-    )
+  return (
+    <div className='max-w-5xl mx-auto pt-10'>
+      <p className='text-lg'>Pattern</p>
+      <div className='flex gap-5 flex-wrap mt-2'>
+        {patterns.map((pattern, i) => (
+          <Link
+            key={i}
+            to={pattern.url}
+            className='px-3.5 py-1.5 rounded bg-gray-200 text-gray-700 hover:bg-gray-300'
+          >
+            {pattern.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
+
+const patterns = [
+  {
+    name: 'Screen split',
+    url: '/screensplit',
+  },
+]
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,6 +6,7 @@ export default function MainRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/screensplit' element={<ScreenSplitPage />} />
     </Routes>
   )
 }
